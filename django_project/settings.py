2bef9dfc7ddc092bd760448025883600c63dc0a4
daniel-grinevich <kgbdannn@gmail.com>
2023-03-18 20:50:51 -0400

    accounts signup & login

--- django_project/settings.py
+++ django_project/settings.py
@@ -129,6 +129,9 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
 
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
